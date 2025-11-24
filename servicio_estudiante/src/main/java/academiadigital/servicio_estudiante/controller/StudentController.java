9b7f6592ae9819b1e1289c4dfde267edc3b58cba
David Poleo <davpoleo@gmail.com>
2025-11-23 23:08:16 -0400

    Falto por agregar el controller - queda por implementar la logica del Swagger, el Logger(Slf4j) y el Testing

---
+++ servicio_estudiante/src/main/java/academiadigital/servicio_estudiante/controller/StudentController.java
@@ -0,0 +1,63 @@
+package academiadigital.servicio_estudiante.controller;
+
+import academiadigital.servicio_estudiante.dto.StudentRequestDto;
+import academiadigital.servicio_estudiante.dto.StudentResponseDto;
+import academiadigital.servicio_estudiante.service.StudentService;
+import academiadigital.servicio_estudiante.util.ApiConstants;
+import jakarta.validation.Valid;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(ApiConstants.CONTROLLER_MAP_BASE)
+@Data
+public class StudentController {
+    private final StudentService studentService;
+
+    @GetMapping(ApiConstants.CONTROLLER_MAP_GET_BY_ID)
+    public ResponseEntity<StudentResponseDto> getStudentById(@PathVariable Long id){
+        return ResponseEntity.ok(studentService.getStudentById(id));
+    }
+
+    @GetMapping(ApiConstants.CONTROLLER_MAP_GET_BY_EMAIL)
+    public ResponseEntity<StudentResponseDto> getStudentByEmail(@PathVariable String email){
+        return ResponseEntity.ok(studentService.getStudentByEmail(email));
+    }
+
+    @GetMapping(ApiConstants.CONTROLLER_MAP_GET_ALL)
+    public ResponseEntity<List<StudentResponseDto>> getAllStudents(){
+        return ResponseEntity.ok(studentService.getAllStudents());
+    }
+
+    @PostMapping(ApiConstants.CONTROLLER_MAP_CREATE)
+    public ResponseEntity<StudentResponseDto> createStudent(
+            @Valid @RequestBody StudentRequestDto request
+            ){
+        StudentResponseDto createdStudent = studentService.createStudent(request);
+        return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
+    }
+
+    @PutMapping(ApiConstants.CONTROLLER_MAP_UPDATE)
+    public ResponseEntity<StudentResponseDto> updateStudent(
+            @PathVariable Long id,
+            @Valid @RequestBody StudentRequestDto request
+    ){
+        StudentResponseDto updatedStudent = studentService.updateStudent(id, request);
+        return ResponseEntity.ok(updatedStudent);
+    }
+
+    @DeleteMapping(ApiConstants.CONTROLLER_MAP_DELETE)
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+}
