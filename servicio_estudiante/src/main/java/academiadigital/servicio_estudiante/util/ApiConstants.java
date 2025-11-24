cd2522021247702584141449c524987432de99ba
David Poleo <davpoleo@gmail.com>
2025-11-23 23:08:10 -0400

    Falto por agregar el controller - queda por implementar la logica del Swagger, el Logger(Slf4j) y el Testing

--- servicio_estudiante/src/main/java/academiadigital/servicio_estudiante/util/ApiConstants.java
+++ servicio_estudiante/src/main/java/academiadigital/servicio_estudiante/util/ApiConstants.java
@@ -6,17 +6,25 @@ public class ApiConstants {
     }
 
     //ERROR VALIDATION MESSAGES
-    public  static final String VALIDATE_ERR_GENERAL = "Error de Validacion";
-    public  static final String VALIDATE_ERR_BLANKS_MESSAGE = "El campo no puede estar vacio";
-    public  static final String VALIDATE_ERR_EMAIL_MESSAGE = "Se debe ingresar un email valido";
-    public  static final String VALIDATE_ERR_EMAIL_NOT_FOUND = "El email no se encuenta registrado en el sistema";
-    public  static final String VALIDATE_ERR_DUPLICATED_RESOURCE = "Conflicto de datos";
-    public  static final String VALIDATE_ERR_SERVER_ERROR = "Error interno del servidor";
-    public  static final String VALIDATE_ERR_UNEXPECTED = "Ocurrio un error inesperado";
+    public static final String VALIDATE_ERR_GENERAL = "Error de Validacion";
+    public static final String VALIDATE_ERR_BLANKS_MESSAGE = "El campo no puede estar vacio";
+    public static final String VALIDATE_ERR_EMAIL_MESSAGE = "Se debe ingresar un email valido";
+    public static final String VALIDATE_ERR_EMAIL_NOT_FOUND = "El email no se encuenta registrado en el sistema";
+    public static final String VALIDATE_ERR_DUPLICATED_RESOURCE = "Conflicto de datos";
+    public static final String VALIDATE_ERR_SERVER_ERROR = "Error interno del servidor";
+    public static final String VALIDATE_ERR_UNEXPECTED = "Ocurrio un error inesperado";
 
     //BUSINESS ERROR MESSAGE
-    public  static final String BUSINESS_ERR_ID_NOT_FOUND = "El ID: '{}' no esta registrado en el sistema";
-    public  static final String BUSINESS_ERR_EMAIL_NOT_FOUND = "El email: '{}' no esta registrado en el sistema";
-    public  static final String BUSINESS_ERR_DUPLICATED_EMAIL = "El email: '{}' ya esta en uso, ingrese uno nuevo.";
+    public static final String BUSINESS_ERR_ID_NOT_FOUND = "El ID: '{}' no esta registrado en el sistema";
+    public static final String BUSINESS_ERR_EMAIL_NOT_FOUND = "El email: '{}' no esta registrado en el sistema";
+    public static final String BUSINESS_ERR_DUPLICATED_EMAIL = "El email: '{}' ya esta en uso, ingrese uno nuevo.";
 
+    //CONSTANTES DEL CONTROLLER
+    public static final String CONTROLLER_MAP_BASE = "/api/v1/students";
+    public static final String CONTROLLER_MAP_CREATE = "/create";
+    public static final String CONTROLLER_MAP_GET_BY_ID = "/{id}";
+    public static final String CONTROLLER_MAP_GET_BY_EMAIL = "email/{email}";
+    public static final String CONTROLLER_MAP_GET_ALL = "/all";
+    public static final String CONTROLLER_MAP_UPDATE = "/update/{id}";
+    public static final String CONTROLLER_MAP_DELETE = "/delete/{id}";
 }
